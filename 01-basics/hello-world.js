d909fca8663934399acc48ae1aa5efb77718a7fd
Johan Nordström <johan@digitalvillage.se>
2023-09-05 20:57:29 +0200

    Our first lines of TypeScript 👶🏻.

---
+++ 01-basics/hello-world.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+console.log("Hello, world!")
+
+let myName: string;
+myName = "Alice";
+myName = "Bob";
+// myName = 42;
+
+let myAge: number;
+myAge = 42;
+myAge = 18;
+// myAge = false;
+
+let needsCoffe: boolean;
+needsCoffe = true;
+needsCoffe = false;
+// needsCoffe = "42";
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+let myName: string = "Alice";
+let myAge: number = 42;
+let needsCoffe: boolean = true;
+*/
+var myName = "Alice"; // Typen blir automatiskt string
+var myAge = 42; // Typen blir automatiskt number
+var needsCoffe = true; // Typen blir automatiskt boolean
---
+++ 01-basics/hello-world.ts
@@ -0,0 +1,32 @@
+/*
+console.log("Hello, world!")
+
+let myName: string;
+myName = "Alice";
+myName = "Bob";
+// myName = 42;
+
+let myAge: number;
+myAge = 42;
+myAge = 18;
+// myAge = false;
+
+let needsCoffe: boolean;
+needsCoffe = true;
+needsCoffe = false;
+// needsCoffe = "42";
+*/
+
+/*
+let myName: string = "Alice";
+let myAge: number = 42;
+let needsCoffe: boolean = true;
+*/
+
+let myName = "Alice";  // Typen blir automatiskt string
+let myAge = 42;        // Typen blir automatiskt number
+let needsCoffe = true; // Typen blir automatiskt boolean
+
+// needsCoffe = null;     // Type 'null' is not assignable to type 'boolean'.ts(2322)
+
+export {};
