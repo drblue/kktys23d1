17f86273fcd4b7ef0c987d0a0f53738b3a335fa7
Johan Nordström <johan@digitalvillage.se>
2023-09-16 21:34:34 +0200

    Copy of `03-todos`.

---
+++ 04-async-todos/src/main.ts
@@ -0,0 +1,65 @@
+import "./assets/scss/app.scss"
+
+// const todosEl = document.querySelector("#todos") as HTMLUListElement
+const todosEl = document.querySelector<HTMLUListElement>("#todos")!
+//     ^?
+const newTodoFormEl = document.querySelector<HTMLFormElement>("#new-todo-form")!
+//     ^?
+
+type Todo = {
+	title: string
+	completed: boolean
+}
+
+// Get JSON of Todos from LocalStorage
+const json = localStorage.getItem('todos') ?? '[]'
+
+// Parse JSON into an array of Todo objects
+const todos: Todo[] = JSON.parse(json)
+
+// Render todos
+const renderTodos = () => {
+	todosEl.innerHTML = todos
+		.map(todo =>
+			`<li class="list-group-item ${todo.completed ? 'completed' : ''}">
+				${todo.title}
+			</li>`
+		)
+		.join('')
+}
+
+// Save todos to localStorage
+const saveTodos = () => {
+	// Convert todos-array to JSON
+	const json = JSON.stringify(todos)
+
+	// Save JSON to localStorage
+	localStorage.setItem('todos', json)
+}
+
+// "Create a new Todo" form
+newTodoFormEl.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!
+//              ^?
+
+	// const newTodo = {
+	// 	title: newTodoTitleEl.value,
+	// 	completed: false,
+	// }
+	// todos.push(newTodo)
+	todos.push({
+		title: newTodoTitleEl.value,
+		completed: false,
+	})
+
+	// Save todos to localStorage
+	saveTodos()
+
+	// Re-render todos
+	renderTodos()
+})
+
+// Render initial list of todos
+renderTodos()
