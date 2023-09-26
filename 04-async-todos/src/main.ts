86ee635911cfb6116642a5d7835c9cc0ab435833
Johan Nordström <johan@digitalvillage.se>
2023-09-26 17:51:14 +0200

    Removed top-level await due to tsconfig being whiny.

--- 04-async-todos/src/main.ts
+++ 04-async-todos/src/main.ts
@@ -1,42 +1,49 @@
-import { getTodos, createTodo } from './api'
-import { Todo } from './todo.types'
-import "./assets/scss/app.scss"
+import { getTodos as TodosAPI_getTodos, createTodo as TodosAPI_createTodos } from "./api";
+import { Todo } from "./todo.types";
+import "./assets/scss/app.scss";
 
-const todosEl = document.querySelector<HTMLUListElement>("#todos")!
-const newTodoFormEl = document.querySelector<HTMLFormElement>("#new-todo-form")!
+const todosEl = document.querySelector<HTMLUListElement>("#todos")!;
+const newTodoFormEl = document.querySelector<HTMLFormElement>("#new-todo-form")!;
+
+// Local variable containing all the todos from the server
+let todos: Todo[] = [];
 
 // Get todos from API
-let todos: Todo[] = await getTodos()
+const getTodos = async () => {
+	// Fetch todos from server and update local copy
+	todos = await TodosAPI_getTodos();
+
+	// Render todos
+	renderTodos();
+};
 
 // Render todos
 const renderTodos = () => {
 	todosEl.innerHTML = todos
-		.map(todo =>
-			`<li class="list-group-item ${todo.completed ? 'completed' : ''}">
+		.map(
+			(todo) =>
+				`<li class="list-group-item ${todo.completed ? "completed" : ""}">
 				${todo.title}
 			</li>`
 		)
-		.join('')
-}
+		.join("");
+};
 
 // "Create a new Todo" form
-newTodoFormEl.addEventListener('submit', async (e) => {
-	e.preventDefault()
+newTodoFormEl.addEventListener("submit", async (e) => {
+	e.preventDefault();
 
-	const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!
+	const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!;
 
 	// POST todo to API
-	await createTodo({
+	await TodosAPI_createTodos({
 		title: newTodoTitleEl.value,
 		completed: false,
-	})
+	});
 
-	// Get the todos from API (including the newly created todo)
-	todos = await getTodos()
+	// Get the todos from API and re-render the list
+	await getTodos();
+});
 
-	// Re-render todos
-	renderTodos()
-})
-
-// Render initial list of todos
-renderTodos()
+// Get the todos from API and render initial list of todos
+getTodos();
