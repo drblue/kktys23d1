6a2963429caaef1c5bb1396c7988d456802ce533
Johan Nordström <johan@digitalvillage.se>
2023-10-10 19:31:30 +0200

    Functional Components with type-safe props.

--- 06-react-todos/src/pages/TodosPage.tsx
+++ 06-react-todos/src/pages/TodosPage.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Spinner from 'react-bootstrap/Spinner'
+import Success from '../components/alerts/Success'
+import Warning from '../components/alerts/Warning'
+import TodoListItem from '../components/TodoListItem'
 import { getTodos as TodosAPI_getTodos } from '../services/TodosAPI'
 import { Todo } from '../types/Todo.types'
 
@@ -27,16 +30,25 @@ const TodosPage = () => {
 			{isLoading && <Spinner />}
 
 			{todos && todos.length > 0 && (
-				<ListGroup className="todolist">
-					{todos.map(todo => (
-						<ListGroup.Item
-							className={''}
-							key={todo.id}
-						>
-							{todo.title}
-						</ListGroup.Item>
-					))}
-				</ListGroup>
+				<>
+					<ListGroup className="todolist mb-4">
+						{todos.map(todo => (
+							<TodoListItem
+								key={todo.id}
+								todo={todo}
+							/>
+						))}
+					</ListGroup>
+
+					<Warning heading="Such todos">
+						<p>Very many</p>
+						<p>Much stress</p>
+					</Warning>
+				</>
+			)}
+
+			{todos && todos.length === 0 && (
+				<Success>Great success!</Success>
 			)}
 		</>
 	)
