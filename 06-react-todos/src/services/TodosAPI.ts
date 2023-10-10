f2b01c083916fa92dae57bece27760bc1fbee441
Johan Nordström <johan@digitalvillage.se>
2023-10-10 17:53:35 +0200

    Improved API-service with Axios instance and base URL.

--- 06-react-todos/src/services/TodosAPI.ts
+++ 06-react-todos/src/services/TodosAPI.ts
@@ -4,12 +4,32 @@
 import axios from 'axios'
 import { Todo } from '../types/Todo.types.ts'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+
+// Create a new axios instance
+const instance = axios.create({
+	baseURL: BASE_URL,
+	timeout: 10000,
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json"
+	}
+})
+
+/**
+ * Make a generic HTTP GET request.
+ *
+ * @param endpoint Endpoint to get
+ * @returns
+ */
+const get = async <T>(endpoint: string) => {
+	const response = await instance.get<T>(endpoint)
+	return response.data
+}
 
 /**
  * Get all todos
  */
-export const getTodos = async () => {
-	const res = await axios.get<Todo[]>(`${BASE_URL}/todos`)
-	return res.data
+export const getTodos = () => {
+	return get<Todo[]>("/todos")
 }
